8b08c89de588cd7208531bdc4bc1dbfec5b4e6ba
jainayushee <jainayushee5@gmail.com>
2022-08-25 21:42:28 +0530

    Add solution

---
+++ ValidPalindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String parsedString="";
+        String reversedString="";
+        for(int i=0; i<s.length(); i++){
+            if(Character.isLetter(s.charAt(i)) ||Character.isDigit(s.charAt(i)) ){
+                parsedString+=Character.toLowerCase(s.charAt(i));
+                reversedString = Character.toLowerCase(s.charAt(i)) + reversedString;
+            }                                
+        }
+        if(reversedString.equals(parsedString))        
+        return true;
+        else
+            return false;
+        
+    }
+}
